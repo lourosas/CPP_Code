346d2fbf328eaeb76c510855b7e8ac7263acfd58
Lou Rosas <lourosas@gmail.com>
2019-08-18 08:15:57 -0700

    08_18_19_0

--- ThreadA.cc
+++ ThreadA.cc
@@ -14,12 +14,15 @@ ThreadA::~ThreadA(){}
 //
 //
 void ThreadA::doWaits
-(std::mutex& m,
- std::condition_variable& cv,
- ThreadZ& tz){
+(
+   std::mutex& m,
+   std::condition_variable& cv,
+   ThreadZ& tz
+){
    std::unique_lock<std::mutex> lk(m);
-	std::cout<<"Waiting...\n";
-	cv.wait(lk, [=]{ return tz.getCount() == 1; });
-	std::cout<<"...finished waiting. value = "<<tz.getCount();
-	tz.setDone(true);
+   std::cout<<"Waiting...\n";
+   cv.wait(lk, [&tz]{ return tz.getCount() == 1; });
+   std::cout<<"...finished waiting. value = "<<tz.getCount()
+     <<std::endl;
+   tz.setDone(true);
 }
