d9a4c6b05611c9d670d1d2c8be3a2cf5a4e488da
lou <lourosas@gmail.com>
2020-07-16 15:43:02 -0700

    07-16-20-01

--- testTree.cc
+++ testTree.cc
@@ -10,10 +10,18 @@ int main(int argc, char* argv[]){
    Tree t;
    time_t timer;
    srand((unsigned)time(&timer));
-   for(int i = 0; i < 10; i++){
-      int value = rand() % 1000;
+   for(int i = 0; i < 100; i++){
+      int value = rand() % 10000;
       std::cout<<"Value = "<<value<<", "<<t.insert(value)
          <<" (size)"<<std::endl;
+   }
+   std::cout<<"Tree:  "<<t;
+   std::cout<<std::endl<<std::endl;
+   srand((unsigned)time(&timer));
+   for(int i = 0; i < 10; i++){
+      int value = rand() % 10000;
+      std::cout<<"Remove Value = "<<value<<", "<<t.remove(value)
+         <<std::endl;
    }
    std::cout<<"Tree:  "<<t;
 }
