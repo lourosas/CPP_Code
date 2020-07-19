3a139c80c45342563b5102fe1a51cbb94f5db8df
lou <lourosas@gmail.com>
2020-07-18 17:01:34 -0700

    07-18-20-04

--- BinaryTree.h
+++ BinaryTree.h
@@ -4,5 +4,91 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+#include <iostream>
+#include <iomanip>
+#include "TreeNode.h"
+
+template <class T>
+class BinaryTree
+{
+   public:
+      BinaryTree();
+      virtual ~BinaryTree();
+      virtual T get(T );
+      virtual int insert(T );
+      virtual int peek(T );
+      virtual int remove(T );
+      std::ostream& print(std::ostream& );
+   private:
+      T get(TreeNode<T>* , T);
+      int insert(TreeNode<T>*&, T);
+      Node<T>* maximum(TreeNode<T>*& );
+      Node<T>* minimum(TreeNode<T>*& );
+      int peek(TreeNode<T>*, T);
+      int remove(TreeNode<T>*, TreeNode<T>*, T);
+      void transplant(TreeNode<T>*&, TreeNode<T>*, TreeNode<T>*);
+      TreeNode<T>* _root;
+      int          _size;
+};
+
+//////////////////////////////////////////////////////////////////////
+/*
+*/
+template <class T>
+BinaryTree<T>::BinaryTree() :
+_root(NULL),
+_size(0){}
+
+/*
+Virtual
+*/
+template <class T>
+BinaryTree<T>::~BinaryTree(){}
+
+/*
+Virtual
+*/
+template <class T>
+T BinaryTree<T>::get(T t){
+   T returnValue;
+
+   returnValue;
+}
+
+/*
+Virtual
+*/
+template <class T>
+int BinaryTree<T>::insert(T t){
+   int return_value = 0;
+   return return_value;
+}
+
+/*
+Virtual
+*/
+template <class T>
+int BinaryTree<T>::peek(T t){
+   return 0;
+}
+
+/*
+Virtual
+*/
+template <class T>
+int BinaryTree<T>::remove(T t){
+   int return_value = 0;
+
+   return return_value;
+}
+
+/*
+*/
+template <class T>
+std::ostream& BinaryTree<T>::print(std::ostream& os){
+   os<<std::endl<<this->_size<<std::endl<<*(this->_root)<<std::endl;
+   return os;
+}
+
 #endif
 //////////////////////////////////////////////////////////////////////
--- TreeNode.h
+++ TreeNode.h
@@ -22,11 +22,12 @@ class TreeNode{
       T _data;
 };
 
+
 //////////////////////////////////////////////////////////////////////
 /*
 */
 template <class T>
-TreeNode::Node(T t) :
+TreeNode<T>::Node(T t) :
 left(NULL),
 right(NULL){
    this->data(t);
@@ -35,5 +36,47 @@ right(NULL){
 /*
 Virtual
 */
+template <class T>
+TreeNode<T>::~Node(){
+   std::cout<<" In the TreeNode Destructor:  "<<this->data()<<", "
+      <<this->left<<", "<<this->right<<std::endl;
+   if(this->left){
+      delete this->left;
+      this->left = NULL;
+   }
+   if(this->right){
+      delete this->right;
+      this=>right = NULL;
+   }
+}
+
+/*
+*/
+template <class T>
+T TreeNode<T>::data(){
+   return this->_data;
+}
+
+/*
+*/
+template <class T>
+std::ostream& TreeNode<T>::print(std::ostream& os){
+   if(this->left){
+      this->left->print(os);
+   }
+   os<<this->data()<<":";
+   if(this->right){
+      this->left->print(os);
+   }
+   return os;
+}
+
+/*
+*/
+template <class T>
+std::ostream& operator<<(std::ostream& os, TreeNode<T>& node){
+   return node.print(os);
+}
+
 #endif
 //////////////////////////////////////////////////////////////////////
