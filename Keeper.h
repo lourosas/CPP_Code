9bd1932a775a2f6b2af30c3228880cdb50f78c1c
lou <lourosas@gmail.com>
2021-01-08 11:12:28 -0700

    01-08-21-01

---
+++ Keeper.h
@@ -0,0 +1,26 @@
+#ifndef KEEPER_H
+#define KEEPER_H
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include "Responder.h"
+
+class Keeper{
+   public:
+      Keeper();
+      Keeper(Responder );
+      ~Keeper();
+      void runTheSystem();
+      void sleepTime(int );
+   protected:
+   private:
+      Responder               _responder;
+      int                     _quit;
+      int                     _sleepTime;
+      std::mutex              _mutex;
+      std::condition_variable _cv;
+};
+#endif
+
---
+++ Responder.h
@@ -0,0 +1,16 @@
+#ifndef RESPONDER_H
+#define RESPONDER_H
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+class Responder{
+   public:
+      Responder();
+      ~Responder();
+   protected:
+   private:
+};
+#endif
