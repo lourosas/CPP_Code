3eacdd9a56eb677f5def2efeefc139f51a2a848c
Lou <lourosas@gmail.com>
2021-01-17 07:24:36 -0700

    01-17-21-00

---
+++ Response.cc
@@ -0,0 +1,22 @@
+#include "Response.h"
+
+/**/
+Response::Response() :
+_quit(0),
+_sleepTime(1000){}
+
+/**/
+Response::~Response(){}
+
+/**/
+void Response::quit(int quit_){
+   this->_quit = quit_;
+}
+
+/**/
+void  Response::sleepTime(int sleep_){
+   this->_sleepTime = sleep_;
+}
+
+/**/
+void Response::trigger(){}
---
+++ Response.h
@@ -0,0 +1,25 @@
+#ifndef RESPONSE_H
+#define RESPONSE_H
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+class Response{
+   public:
+      Response();
+      virtual ~Response();
+      void quit(int );
+      void sleepTime(int );
+      void trigger();
+   protected:
+   private:
+      void run();
+      int                      _quit;
+      int                      _sleepTime;
+      std::mutex               _mutex;
+      std::condition_variable  _cv;
+};
+
+#endif
