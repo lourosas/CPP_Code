22bcd6c685e57a7c3e407c0c7a79fc2fb5eab1a8
Lou <lourosas@gmail.com>
2021-12-05 10:39:01 -0700

    12-05-21-02

--- HashTable/code/GenericHashElement.h
+++ HashTable/code/GenericHashElement.h
@@ -12,8 +12,32 @@ class GenericHashElement{
       enum{EMPTY, DELETED, SET};
       int storeValue;
       Value value;
+      virtual std::ostream& print(std::ostream& ) const;
    protected:
    private:
 };
+template<class Value>
+std::ostream& operator<<(std::ostream& os,
+                                    GenericHashElement<Value>& elem);
+template<class Value>
+std::ostream& operator<<(std::ostream& os,
+                              const GenericHashElement<Value>& elem);
+
+/*
+virtual
+*/
+template<class Value>
+std::ostream& GenericHashElement<Value>::print(std::ostream& os)const{
+   os<<"("<<this->storeValue<<", "<<this->value<<")";
+   return os;
+}
+
+/////////////////////////Function Definitions/////////////////////////
+template<class Value>
+std::ostream& operator<<(std::ostream& os,
+                                     GenericHashElement<Value>& elem)
+{
+   return (elem.print(os));
+}
 #endif
 //////////////////////////////////////////////////////////////////////
