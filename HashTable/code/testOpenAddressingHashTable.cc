90e57abf5909f044f21724a8eca0c5ea0f338adb
Lou <lourosas@gmail.com>
2021-12-24 07:22:50 -0700

    12-24-21-00

--- HashTable/code/testOpenAddressingHashTable.cc
+++ HashTable/code/testOpenAddressingHashTable.cc
@@ -1,3 +1,19 @@
+/*
+ * This file is part of the CPP_Code Dsistribution
+ * Copyright (c) 2021 Lou Rosas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <iostream>
 #include <cstdlib>
 #include "Integer.h"
@@ -52,7 +68,7 @@ int main(){
    std::cout<<std::endl<<oht.contains(test1)<<std::endl<<std::endl;
 
    std::cout<<oht<<std::endl;
-   
+
 
    oht.insert(test, test);
    std::cout<<oht<<std::endl;
