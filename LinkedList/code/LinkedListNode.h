2c0e1d138f7ea2770d75022d384c8838ac6d7249
Lou <lourosas@gmail.com>
2021-12-27 13:08:20 -0700

    12-27-21-03

---
+++ LinkedList/code/LinkedListNode.h
@@ -0,0 +1,32 @@
+/*
+A generic LinkedListNode class by Lou Rosas
+*/
+/*
+ * This file is part of the CPP_Code Dsistribution
+ * Copyright (c) 2021 Lou Rosas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+#ifndef LINKED_LIST_NODE_H
+#define LINKED_LIST_NODE_H
+
+template <class T>
+class LinkedListNode{
+   public:
+      T data;
+      LinkedListNode<T>* next;
+};
+
+#endif
