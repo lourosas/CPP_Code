9d6945883797f5b0db722ea4cfb7adb3ffc7e94b
Lou Rosas <lourosas@gmail.com>
2026-02-07 15:03:15 -0700

    Update Sieve.h

--- Sieve.h
+++ Sieve.h
@@ -1,30 +1,30 @@
-  1 //////////////////////////////////////////////////////////////////////
-  2 #ifndef SIEVE_H
-  3 #define SIEVE_H
-  4
-  5 #include <iostream>
-  6 #include <iomanip>
-  7 #include <string>
-  8
-  9 using namespace std;
- 10
- 11 class Sieve{
- 12    public:
- 13       Sieve(int );
- 14       ~Sieve();
- 15       std::ostream& print(std::ostream& );
- 16       void findPrimes();
- 17    protected:
- 18    private:
- 19       int   _utai; //Up to and including
- 20       int*  _indices;
- 21       long* _primes;
- 22       int   _numberOfPrimes;
- 23       void  findIndices();
- 24       void  findNumberOfPrimes();
- 25       void  initializeIndices();
- 26       void  setPrimes();
- 27 };
- 28 std::ostream& operator<<(std::ostream& os, Sieve& s);
- 29 #endif
- 30 //////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+#ifndef SIEVE_H
+#define SIEVE_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+class Sieve{
+   public:
+      Sieve(int );
+      ~Sieve();
+      std::ostream& print(std::ostream& );
+      void findPrimes();
+   protected:
+   private:
+      int   _utai; //Up to and including
+      int*  _indices;
+      long* _primes;
+      int   _numberOfPrimes;
+      void  findIndices();
+      void  findNumberOfPrimes();
+      void  initializeIndices();
+      void  setPrimes();
+};
+std::ostream& operator<<(std::ostream& os, Sieve& s);
+#endif
+//////////////////////////////////////////////////////////////////////
